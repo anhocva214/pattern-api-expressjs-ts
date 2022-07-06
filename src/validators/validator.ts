024df18976ac03ebe02796ec73ed27b89544b1c0
anhocva214 <anhocva214@gmail.com>
2022-07-06 19:39:32 +0700

    update validator feature

--- src/validators/validator.ts
+++ src/validators/validator.ts
@@ -25,19 +25,27 @@ export default class Validator {
             objValidate.map(async item => {
                 await Promise.all(
                     item.rules.map( async rule => {
+                        
                         let value, ruleName;
                         if (rule.indexOf(":") >= 0) {
-                            value = rule.split(":")[1]; 
+                            value = rule.split(":")[1];
                             ruleName = rule.split(":")[0]
                         }
-                        else{
+                        else {
                             ruleName = rule
                         }
-                        
-                        let {msg, error} = await this.rules()[ruleName as any](this.obj, item.field, value, this.lang)
-                        if (error){
-                            this.addError(item.field as any, msg)
-                            // item.rules.length = item.rules.indexOf(rule)
+
+                        if ((rule != 'required' && !!this.obj?.[item.field]) || rule == 'optional') {
+                            let { msg, error } = await this.rules()[ruleName as any](this.obj, item.field, value, this.lang)
+                            if (error) {
+                                this.addError(item.field as any, msg)
+                            }
+                        }
+                        else{
+                            let { msg, error } = await this.rules()['required'](this.obj, item.field, value, this.lang)
+                            if (error) {
+                                this.addError(item.field as any, msg)
+                            }
                         }
                     })
                 )
@@ -48,8 +56,11 @@ export default class Validator {
     private addError(field: TFieldname, message: string) {
         if (Object.keys(this.errors).indexOf(field) < 0) {
             this.errors[field] = []
-         }
+        }
         this.errors[field].push(message.replace(":field", trans.validator[this.lang].fieldname[field] || field))
+        let messages = this.errors[field];
+        let messagesSet = new Set(messages)
+        this.errors[field] = Array.from(messagesSet)
     }
 
     hasError(){
@@ -57,7 +68,7 @@ export default class Validator {
     }
 
     private async required(obj: IObj, field: string, value: any, lang: TLang) {        
-        if (!obj[field]) {
+        if (!obj?.[field]) {
             return {
                 error: true,
                 msg: `:field ${trans.validator[lang].message.is_required}`
@@ -72,9 +83,10 @@ export default class Validator {
     private async unique(obj: IObj, field: string, value: any, lang: TLang) {
         let values = value.split(",")
         let fieldCol = values[1];
-        let ignore = values[2];
+        let tableName = value.split(",")[0];
+        // let ignore = values[2];
 
-        let item = await model(value.split(",")[0]).findOne({[fieldCol]: obj[field]})
+        let item = await model(tableName).findOne({[fieldCol]: obj[field]})
 
         if (!!item){
             return {
@@ -114,12 +126,43 @@ export default class Validator {
         }
     }
 
+    private async only(obj: IObj, field: string, value: any, lang: TLang){
+        let inputValue = obj[field];
+        let onlyValues: string[] = value.split(",");
+
+        if (onlyValues.indexOf(inputValue) < 0){
+            return {
+                error: true,
+                msg: `:field ${trans.validator[lang].message.is_not_exist}`
+            }
+        }
+        else return {
+            error: false,
+            msg: ""
+        }
+    }
+
+    private async optional(obj: IObj, field: string, value: any, lang: TLang){
+        if (Object.keys(obj).indexOf(field) < 0 ){
+            return {
+                error: true,
+                msg: `:field ${trans.validator[lang].message.is_undefined}`
+            }
+        }
+        else return {
+            error: false,
+            msg: ""
+        }
+    }
+
     private rules(): any {
         return {
             required: this.required,
             isNumeric: this.isNumeric,
             isEmail: this.isEmail,
-            unique: this.unique
+            unique: this.unique,
+            only: this.only,
+            optional: this.optional
         }
     }
 }
