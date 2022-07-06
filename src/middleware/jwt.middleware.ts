6aef2a6f688019f3f077de15047c0705c0a3f94e
anhocva214 <anhocva214@gmail.com>
2022-07-06 19:40:18 +0700

    update middleware

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -11,28 +11,33 @@ const usersRepository = new UsersRepository()
 
 export default function middleware(role: string) {
     return async (req: Request, res: Response, next: NextFunction) => {
-        try{
-            let authorization: string = req.headers.authorization as string;
-            let accessToken = authorization.split(" ")[1].trim();
-          
-    
+        let authorization: string = req.headers.authorization as string;
+        let accessToken = authorization?.split(" ")?.[1]?.trim();
+        try {
             let payload = jwtRepository.verifyAccessToken(accessToken)
-            
-            if (payload.role != role) return res.status(401).send({message: 'Role is not allowed'})
 
-            let token = await tokensRepository.findOne({module_id: payload._id, payload: accessToken})
+            if (!!role && payload.role != role) return res.status(403).send({ message: 'Role is not allowed' })
 
-            if (!token) return res.status(401).send({message: 'access_token is inactive'})
+            let token = await tokensRepository.findOne({ module_id: payload._id, payload: accessToken })
 
-            req.user = await usersRepository.findOne({_id: payload._id})
+            if (!token) return res.status(403).send({ message: 'access_token is inactive' })
+
+            req.user = await usersRepository.findOne({ _id: payload._id })
             req.tokenId = token._id;
-    
+
             next()
         }
         catch (err: any) {
-            console.log(err)
-            if (err.message == 'invalid signature') return res.status(401).send({message: "access_token is invalid"})
-            else return res.status(400).send({message: "access_token is required"})
+            console.log(err.message)
+            if (err.message == 'jwt expired') {
+                await usersRepository.deleteOne({ payload: accessToken })
+                return res.status(403).send({ message: "access_token is expired" })
+            }
+            else if (err.message == 'jwt must be provided'){
+                return res.status(403).send({ message: "access_token must be provided" })
+            }
+            else if (err.message == 'invalid signature') return res.status(403).send({ message: "access_token is invalid" })
+            else return res.status(403).send({ message: "access_token is required" })
         }
 
     }
