69cfd745fba8ad3ebeb3f153834c3e84bc5fd3c0
anhocva214 <anhocva214@gmail.com>
2022-10-11 14:31:43 +0700

    update

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,7 +2,6 @@ import { connect } from 'mongoose';
 
 export async function connectDB(): Promise<void> {
     await connect('mongodb+srv://test:test@cluster0.baswu.mongodb.net/test?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+    
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
-import Logger from '@services/logger.service';
-
+// import Logger from '@services/logger.service';
+import logger from 'jet-logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-const logger = new Logger()
+// const logger = new Logger()
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
