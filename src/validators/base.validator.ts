8a9604d6518396ef4fd43ed7bd6aff1d7ff63c7c
anhocva214 <anhocva214@gmail.com>
2022-11-25 00:41:56 +0700

    validate: update unique

--- src/validators/base.validator.ts
+++ src/validators/base.validator.ts
@@ -5,9 +5,10 @@ import Validator from './validator';
 
 export default class BaseValidator {
 
+    // Should run after middleware
     protected validate(objValidate: IObjValidate[]) {
         return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-            let validator = new Validator(req.body, req.lang)
+            let validator = new Validator(req.body, req.lang, req.user)
             await validator.validate(objValidate)
             
             let payload : any = {}
